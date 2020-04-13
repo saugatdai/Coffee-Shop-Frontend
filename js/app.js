659ce586af1fdcc0092488c6db83f45cc7e27ffa
saugatdai <saugatdai@gmail.com>
2020-04-13 20:21:49 +0545

    Work section design

--- js/app.js
+++ js/app.js
@@ -19,6 +19,25 @@ function eventListeners() {
     document.querySelector('.video__switch').addEventListener('click', () => {
         ui.videoControls();
     });
+    // submit the form
+    document.querySelector('.drink-form').addEventListener('submit', event => {
+        event.preventDefault();
+        const name = document.querySelector('.input-name').value;
+        const lastname = document.querySelector('.input-lastname').value;
+        const email = document.querySelector('.input-email').value;
+
+        let value = ui.checkEmpty(name, lastname, email);
+
+        if (value) {
+            let customer = new Customer(name, lastname, email);
+            ui.addCustomer(customer);
+            ui.showFeedback('Customer added to the list', 'success');
+            ui.clearFields();
+        } else {
+            ui.showFeedback('some form values are empty', 'error');
+        }
+
+    });
 }
 
 function UI() {}
@@ -43,4 +62,56 @@ UI.prototype.videoControls = () => {
         btn.classList.add('btnSlide');
         document.querySelector('.video__item').pause();
     }
+}
+
+// check for empty values
+UI.prototype.checkEmpty = function(firstname, lastname, email) {
+
+    return firstname !== '' && lastname !== '' && email !== '';
+}
+
+UI.prototype.showFeedback = function(text, type) {
+    const feedback = document.querySelector('.drink-form__feedback');
+    if (type === 'success') {
+        feedback.classList.add('success');
+        feedback.innerText = text;
+        this.removeAlert('success');
+    } else if (type === 'error') {
+        feedback.classList.add('error');
+        feedback.innerText = text;
+        this.removeAlert('error');
+    }
+}
+
+// remove alert
+UI.prototype.removeAlert = function(type) {
+    setTimeout(function() {
+        document.querySelector('.drink-form__feedback').classList.remove(type);
+    }, 3000);
+}
+
+// add Customer
+UI.prototype.addCustomer = function(customer) {
+    console.log(customer);
+    const images = [1, 2, 3, 4, 5];
+    let random = Math.floor(Math.random() * images.length);
+    const div = document.createElement('div');
+    div.classList.add('person');
+    div.innerHTML = `<img src='img/person-${random}.jpeg' class='person__thumbnail'>
+                    <h4 class="person__name">${customer.name}</h4>
+                    <h4 class="person__last-name">${customer.lastname}</h4>`;
+    document.querySelector('.drink-card__list').appendChild(div);
+}
+
+// clear fields
+UI.prototype.clearFields = function() {
+    document.querySelector('.input-name').value = '';
+    document.querySelector('.input-lastname').value = '';
+    document.querySelector('.input-email').value = '';
+}
+
+function Customer(name, lastname, email) {
+    this.name = name;
+    this.lastname = lastname;
+    this.email = email;
 }
